7cf4a1ff36e4fdd4530d08bd283b99aa8a370c82
Max Wagner <wagnerm@github.com>
2023-03-24 19:43:10 +0000

    Set an output for the Vault token retrieval failure message
    With this we can inspect the error message that is output from Vault
    to see if it's a bunch of 403s or 500s.

--- src/auth.js
+++ src/auth.js
@@ -115,6 +115,7 @@ async function getClientToken(client, method, path, payload) {
         response = await client.post(`v1/auth/${path}/login`, options);
     } catch (err) {
         if (err instanceof got.HTTPError) {
+            core.setOutput("tokenerrormessage", err.message);
             throw Error(`failed to retrieve vault token. code: ${err.code}, message: ${err.message}, vaultResponse: ${JSON.stringify(err.response.body)}`)
         } else {
             throw err
